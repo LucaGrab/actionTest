f226954e74416cc6d47af92335702a77f1230441
Daniel Schäfer <daniel.schaefer05@sap.com>
2024-02-22 00:22:08 +0100

    addMistake

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function addiere(a, b) {
 }
 console.log(addiere(1, 2)); // 3
 function subtrahiere(a, b) {
-    return a - b;
+    return a + b;
 }
 console.log(subtrahiere(1, 2)); // -1
 module.exports = addiere;
